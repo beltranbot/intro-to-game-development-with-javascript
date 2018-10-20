fdee9c5588f966633fc97eba96bf958392d4bda1
hx <beltranbot@gmail.com>
2018-10-19 23:14:30 -0500

    collision detection between the ball and paddle

--- src/ball.js
+++ src/ball.js
@@ -1,12 +1,14 @@
 class Ball {
   constructor(game) {
     this.image = document.getElementById('img_ball')
-    this.speed = { x: 2, y: 2 }
+    this.speed = { x: 4, y: 4 }
     this.position = { x: 10, y: 10 }
     this.size = 16
 
     this.gameWidth = game.gameWidth
     this.gameHeight = game.gameHeight
+
+    this.game = game
   }
 
   draw(ctx) {
@@ -29,6 +31,19 @@ class Ball {
 
     if ((this.position.y + this.size) > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y
+    }
+
+    // check collision with paddle
+    let bottomOfBall = this.position.y + this.size
+    let topOfPaddle = this.game.paddle.position.y
+    let leftSideOfPaddle = this.game.paddle.position.x
+    let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width
+
+    if (bottomOfBall >= topOfPaddle
+      && this.position.x >= leftSideOfPaddle
+      && this.position.x + this.size <= rightSideOfPaddle) {
+      this.speed.y = -this.speed.y
+      this.position.y = this.game.paddle.position.y - this.size
     }
   }
 }
