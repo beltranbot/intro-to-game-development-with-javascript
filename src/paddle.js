ead2f8a1a5241c44c7851d30f22de975f2831333
hx <beltranbot@gmail.com>
2018-10-19 22:24:10 -0500

    moving the paddle

--- src/paddle.js
+++ src/paddle.js
@@ -4,6 +4,11 @@ class Paddle {
     this.width = 150;
     this.height = 20;
 
+    this.maxSpeed = 7
+    this.speed = 0
+
+    this.gameWidth = gameWidth
+
     this.position = {
       x: gameWidth / 2 - this.width / 2,
       y: gameHeight - this.height - 10
@@ -21,9 +26,24 @@ class Paddle {
     )
   }
 
+  moveLeft () {
+    this.speed = -this.maxSpeed
+  }
+
+  moveRight () {
+    this.speed = this.maxSpeed
+  }
+  
   update(deltaTime) {
     if (!deltaTime) return
-    this.position.x += 5 / deltaTime
+
+    this.position.x += this.speed
+    
+    if (this.position.x < 0) {
+      this.position.x = 0
+    } else if (this.position.x > (this.gameWidth - this.width)) {
+      this.position.x = (this.gameWidth - this.width)
+    }
   }
 }
 
